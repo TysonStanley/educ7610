984132c4e96befafa00ce8367579ed9b36646099
TysonStanley <t.barrett@aggiemail.usu.edu>
2018-08-05 16:53:50 -0600

    add odds ratio function

---
+++ R/oddsratios.R
@@ -0,0 +1,15 @@
+#' Odds Ratios
+#'
+#' This function provides the exponentiated coefficients and confidence intervals
+#' from a logistic regression model.
+#'
+#' @param model The generalized linear model object (from \code{glm()})
+#'
+#' @importFrom stats coef confint
+#'
+#' @export
+
+oddsratios <- function(model){
+  cbind(exp(stats::coef(model)),
+        exp(stats::confint(model)))
+}
