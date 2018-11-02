4b052b191a9225e74ad12379a6b60db852c3c32d
Tyson S. Barrett <t.barrett88@gmail.com>
2018-11-01 18:45:28 -0600

    update name to educ7610

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
-.onAttach <- function(libname = find.package("rlm"), pkgname = "rlm") {
+.onAttach <- function(libname = find.package("educ7610"), pkgname = "educ7610") {
 
   ## get potential conflicts and filter out unncessary ones
   confs <- search_conflicts() %>%
@@ -12,22 +12,22 @@
     helper_msg <- ""
   } else {
     confs_msg <- text_col(paste0(crayon::yellow(cli::symbol$cross),
-                                 " The rlm::", rownames(confs), "() function has the same name as ",
+                                 " The educ7610::", rownames(confs), "() function has the same name as ",
                                  gsub("package:", "", confs$rowname), "::", rownames(confs), " (",
                                  sapply(paste0(gsub("package:", "", confs$rowname), "::", rownames(confs)),
                                         function(x) class(eval(parse(text = x)))[1]), ")\n"))
-    helper_msg <- text_col(crayon::italic("   Consider using `rlm::` for each function call."))
+    helper_msg <- text_col(crayon::italic("   Consider using `educ7610::` for each function call."))
   }
 
-  packageStartupMessage(text_col(cli::rule(left = paste0("rlm ", rlm_version("rlm")),
+  packageStartupMessage(text_col(cli::rule(left = paste0("educ7610 ", rlm_version("educ7610")),
                                            right = "learn more at tysonbarrett.com")),
-                        text_col(paste0("\n", crayon::green(cli::symbol$tick), " rlm attached\n")),
+                        text_col(paste0("\n", crayon::green(cli::symbol$tick), " educ7610 attached\n")),
                         confs_msg,
                         helper_msg)
 
 }
 
-.onLoad <- function(libname = find.package("rlm"), pkgname = "rlm"){
+.onLoad <- function(libname = find.package("educ7610"), pkgname = "educ7610"){
   if(getRversion() >= "2.15.1") {
     utils::globalVariables(c(".", "pred", "resid"))
   }
