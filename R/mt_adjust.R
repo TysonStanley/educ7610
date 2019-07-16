77bd1062ada60be5e2ebcf3686274dc6d95597e8
Tyson S. Barrett <t.barrett88@gmail.com>
2019-07-15 18:30:20 -0600

    better docs for mt_adjust

--- R/mt_adjust.R
+++ R/mt_adjust.R
@@ -3,7 +3,7 @@
 #' Obtain the regression results with a multiple testing adjustment
 #'
 #' @param model the regression model
-#' @param adjustment the type of adjustment
+#' @param adjustment the type of adjustment (any that `stats::p.adjust()` can handle)
 #'
 #' @return a data.frame
 #'
